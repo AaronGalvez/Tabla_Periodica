2210b8bf9d623ea2a69673741d8034e58cd72ae1
JossWMN <joss.mendieta@utp.ac.pa>
2025-11-18 00:38:48 -0500

    Modificaciones completas de cada archivo

--- Tabla_periodica.js
+++ Tabla_periodica.js
@@ -1,145 +1,133 @@
-function crearElemento(simbolo, nombre, tipo) {
-    const enlace = document.createElement("a");
-    enlace.textContent = simbolo;
-    enlace.href = "https://es.wikipedia.org/wiki/" + nombre;
 
-    const elementos = document.createElement("td");
-    elementos.classList.add("elementos", tipo);
-    elementos.appendChild(enlace);
-    
-    return elementos;
-}
-function crearElemento2(simbolo2, nombre2, tipo2) {
-    const enlace2 = document.createElement("a");
-    enlace2.textContent = simbolo2;
-    enlace2.href = "https://es.wikipedia.org/wiki/" + nombre2;
+function crearElemento(simbolo, nombre, tipo, col, row) {
+  const enlace = document.createElement("a");
+  enlace.textContent = simbolo;
+  enlace.href = "https://es.wikipedia.org/wiki/" + nombre;
 
-    const elementos2 = document.createElement("tr");
-    elementos2.classList.add("elementos2", tipo2);
-    elementos2.appendChild(enlace2);
-    
-    return elementos2;
+  const elemento = document.createElement("div");
+  elemento.classList.add("elementos", tipo);
+  elemento.appendChild(enlace);
+
+  // posicionar en la grid
+  if (col) elemento.style.gridColumn = String(col);
+  if (row) elemento.style.gridRow = String(row);
+
+  return elemento;
 }
 
-const Seccion1 = document.getElementById("Seccion1");
-const Seccion2 = document.getElementById("Seccion2");
-const Contenedor2 = document.getElementById("Contenedor2");
-const Seccion4 = document.getElementById("Seccion4");
-const Seccion5 = document.getElementById("Seccion5");
-const Seccion6 = document.getElementById("Seccion6");
-const Seccion7 = document.getElementById("Seccion7");
-const Seccion8 = document.getElementById("Seccion8");
-Seccion1.className = "section1";
-Seccion2.className = "section2";
-Contenedor2.className = "Contenedor2";
-Seccion4.className = "section4";
-Seccion5.className = "section5";
-Seccion6.className = "section6";
-Seccion7.className = "section7";
-Seccion8.className = "section8";
+const contenedor = document.getElementById("periodica");
+contenedor.className = "periodica-grid";
 
+// Fila 1
+contenedor.appendChild(crearElemento("H", "Hidrógeno", "seccion1", 1, 1));
+contenedor.appendChild(crearElemento("He", "Helio", "seccion8", 18, 1));
 
+// Fila 2
+contenedor.appendChild(crearElemento("Li", "Litio", "seccion1", 1, 2));
+contenedor.appendChild(crearElemento("Be", "Berilio", "seccion2", 2, 2));
 
-//Seccion 1
-Seccion1.appendChild(crearElemento("H", "Hidrógeno", "H"));
-Seccion1.appendChild(crearElemento("Li", "Litio", "Li"));
-Seccion1.appendChild(crearElemento("Na", "Sodio", "Na"));
-Seccion1.appendChild(crearElemento("K", "Potasio", "K"));
-Seccion1.appendChild(crearElemento("Rb", "Rubidio", "Rb"));
-Seccion1.appendChild(crearElemento("Cs", "Césio", "Cs"));
-Seccion1.appendChild(crearElemento("Fr", "Francio", "Fr"));
+contenedor.appendChild(crearElemento("B", "Boro", "seccion5", 13, 2));
+contenedor.appendChild(crearElemento("C", "Carbono", "seccion6", 14, 2));
+contenedor.appendChild(crearElemento("N", "Nitrógeno", "seccion6", 15, 2));
+contenedor.appendChild(crearElemento("O", "Oxígeno", "seccion6", 16, 2));
+contenedor.appendChild(crearElemento("F", "Flúor", "seccion7", 17, 2));
+contenedor.appendChild(crearElemento("Ne", "Neón", "seccion8", 18, 2));
 
-// Seccion 2
-Seccion2.appendChild(crearElemento("Be", "Berilio", "Be"));
-Seccion2.appendChild(crearElemento("Mg", "Magnesio", "Mg"));
-Seccion2.appendChild(crearElemento("Ca", "Calcio", "Ca"));
-Seccion2.appendChild(crearElemento("Sr", "Estroncio", "Sr"));
-Seccion2.appendChild(crearElemento("Ba", "Bario", "Ba"));
-Seccion2.appendChild(crearElemento("Ra", "Radio", "Ra"));
-//Elementos del contenedor 2 (Seccion 3)
-Contenedor2.appendChild(crearElemento2("Sc", "Escandio", "Sc"));
-Contenedor2.appendChild(crearElemento2("Ti", "Titanio", "Ti"));
-Contenedor2.appendChild(crearElemento2("V", "Vanadio", "V"));
-Contenedor2.appendChild(crearElemento2("Cr", "Cromo", "Cr"));
-Contenedor2.appendChild(crearElemento2("Mn", "Manganeso", "Mn"));
-Contenedor2.appendChild(crearElemento2("Fe", "Hierro", "Fe"));
-Contenedor2.appendChild(crearElemento2("Co", "Cobalto", "Co"));
-Contenedor2.appendChild(crearElemento2("Ni", "Níquel", "Ni"));
-Contenedor2.appendChild(crearElemento2("Cu", "Cobre", "Cu"));
-Contenedor2.appendChild(crearElemento2("Zn", "Zinc", "Zn"));
-Contenedor2.appendChild(crearElemento2("Y", "Itrio", "Y"));
-Contenedor2.appendChild(crearElemento2("Zr", "Circonio", "Zr"));
-Contenedor2.appendChild(crearElemento("Nb", "Niobio", "Nb"));
-Contenedor2.appendChild(crearElemento("Mo", "Molibdeno", "Mo"));
-Contenedor2.appendChild(crearElemento("Tc", "Tecnecio", "Tc"));
-Contenedor2.appendChild(crearElemento("Ru", "Rutenio", "Ru"));
-Contenedor2.appendChild(crearElemento("Rh", "Rodio", "Rh"));
-Contenedor2.appendChild(crearElemento("Pd", "Paladio", "Pd"));
-Contenedor2.appendChild(crearElemento("Ag", "Plata", "Ag"));
-Contenedor2.appendChild(crearElemento("Cd", "Cadmio", "Cd"));
-Contenedor2.appendChild(crearElemento("Lu", "Lutecio", "Lu"));
-Contenedor2.appendChild(crearElemento("Hf", "Hafnio", "Hf"));
-Contenedor2.appendChild(crearElemento("Ta", "Tantalio", "Ta"));
-Contenedor2.appendChild(crearElemento("W", "Wolframio", "W"));
-Contenedor2.appendChild(crearElemento("Re", "Renio", "Re"));
-Contenedor2.appendChild(crearElemento("Os", "Osmio", "Os"));
-Contenedor2.appendChild(crearElemento("Ir", "Iridio", "Ir"));
-Contenedor2.appendChild(crearElemento("Pt", "Platino", "Pt"));
-Contenedor2.appendChild(crearElemento("Au", "Oro", "Au"));
-Contenedor2.appendChild(crearElemento("Hg", "Mercurio", "Hg"));
-Contenedor2.appendChild(crearElemento("Lr", "Lawrencio", "Lr"));
-Contenedor2.appendChild(crearElemento("Rf", "Rutherfordio", "Rf"));
-Contenedor2.appendChild(crearElemento("Db", "Dubnio", "Db"));
-Contenedor2.appendChild(crearElemento("Sg", "Seaborgio", "Sg"));
-Contenedor2.appendChild(crearElemento("Bh", "Bohrio", "Bh"));
-Contenedor2.appendChild(crearElemento("Hs", "Hassio", "Hs"));
-Contenedor2.appendChild(crearElemento("Mt", "Meitnerio", "Mt"));
-Contenedor2.appendChild(crearElemento("Ds", "Darmstatio", "Ds"));
-Contenedor2.appendChild(crearElemento("Rg", "Roentgenio", "Rg"));
-Contenedor2.appendChild(crearElemento("Cn", "Copernicio", "Cn"));
-//yo
-//Seccion 4
-Seccion4.appendChild(crearElemento("Al", "Aluminio", "Al"));
-Seccion4.appendChild(crearElemento("Ga", "Galio", "Ga"));
-Seccion4.appendChild(crearElemento("In", "Indio", "In"));
-Seccion4.appendChild(crearElemento("Sn", "Estaño", "Sn"));
-Seccion4.appendChild(crearElemento("Ti", "Talio", "Ti"));
-Seccion4.appendChild(crearElemento("Pb", "Plomo", "Pb"));
-Seccion4.appendChild(crearElemento("Bi", "Bismuto", "Bi"));
-Seccion4.appendChild(crearElemento("Nh", "Nihonio", "Nh"));
-Seccion4.appendChild(crearElemento("Fl", "Flerovio", "Fl"));
-Seccion4.appendChild(crearElemento("Mc", "Moscovio", "Mc"));
-Seccion4.appendChild(crearElemento("Lv", "Livermorio", "Lv"));
+// Fila 3
+contenedor.appendChild(crearElemento("Na", "Sodio", "seccion1", 1, 3));
+contenedor.appendChild(crearElemento("Mg", "Magnesio", "seccion2", 2, 3));
 
-//Seccion 5
-Seccion5.appendChild(crearElemento("B", "Boro", "B"));
-Seccion5.appendChild(crearElemento("Si", "Silicio", "Si"));
-Seccion5.appendChild(crearElemento("Ge", "Germanio", "Ge"));
-Seccion5.appendChild(crearElemento("As", "Arsénico", "As"));
-Seccion5.appendChild(crearElemento("Sb", "Antimonio", "Sb"));
-Seccion5.appendChild(crearElemento("Te", "Telurio", "Te"));
-Seccion5.appendChild(crearElemento("Po", "Polonio", "Po"));
+contenedor.appendChild(crearElemento("Al", "Aluminio", "seccion4", 13, 3));
+contenedor.appendChild(crearElemento("Si", "Silicio", "seccion5", 14, 3));
+contenedor.appendChild(crearElemento("P", "Fósforo", "seccion6", 15, 3));
+contenedor.appendChild(crearElemento("S", "Azufre", "seccion6", 16, 3));
+contenedor.appendChild(crearElemento("Cl", "Cloro", "seccion7", 17, 3));
+contenedor.appendChild(crearElemento("Ar", "Argón", "seccion8", 18, 3));
 
-//Seccion 6
-Seccion6.appendChild(crearElemento("C", "Carbono", "C"));
-Seccion6.appendChild(crearElemento("N", "Nitrógeno", "N"));
-Seccion6.appendChild(crearElemento("O", "Oxígeno", "O"));
-Seccion6.appendChild(crearElemento("P", "Fósforo", "P"));
-Seccion6.appendChild(crearElemento("S", "Azufre", "S"));
-Seccion6.appendChild(crearElemento("Se", "Selenio", "Se"));
-//Seccion 7
-Seccion7.appendChild(crearElemento("F", "Fluor", "F"));
-Seccion7.appendChild(crearElemento("Cl", "Cloro", "Cl"));
-Seccion7.appendChild(crearElemento("Br", "Bromo", "Br"));
-Seccion7.appendChild(crearElemento("I", "Yodo", "I"));
-Seccion7.appendChild(crearElemento("At", "Astato", "At"));
-Seccion7.appendChild(crearElemento("Ts", "Teneso", "Ts"));
+// Fila 4
+contenedor.appendChild(crearElemento("K", "Potasio", "seccion1", 1, 4));
+contenedor.appendChild(crearElemento("Ca", "Calcio", "seccion2", 2, 4));
+contenedor.appendChild(crearElemento("Sc", "Escandio", "seccion3", 3, 4));
 
-//Seccion 8
-Seccion8.appendChild(crearElemento("He", "Helio", "He"));
-Seccion8.appendChild(crearElemento("Ne", "Neón", "Ne"));
-Seccion8.appendChild(crearElemento("Ar", "Argón", "Ar"));
-Seccion8.appendChild(crearElemento("Kr", "Kriptón", "Kr"));
-Seccion8.appendChild(crearElemento("Xe", "Xenón", "Xe"));
-Seccion8.appendChild(crearElemento("Rn", "Radón", "Rn"));
-Seccion8.appendChild(crearElemento("Og", "Oganesón", "Og"));
+contenedor.appendChild(crearElemento("Ti", "Titanio", "seccion3", 4, 4));
+contenedor.appendChild(crearElemento("V", "Vanadio", "seccion3", 5, 4));
+contenedor.appendChild(crearElemento("Cr", "Cromo", "seccion3", 6, 4));
+contenedor.appendChild(crearElemento("Mn", "Manganeso", "seccion3", 7, 4));
+contenedor.appendChild(crearElemento("Fe", "Hierro", "seccion3", 8, 4));
+contenedor.appendChild(crearElemento("Co", "Cobalto", "seccion3", 9, 4));
+contenedor.appendChild(crearElemento("Ni", "Níquel", "seccion3", 10, 4));
+contenedor.appendChild(crearElemento("Cu", "Cobre", "seccion3", 11, 4));
+contenedor.appendChild(crearElemento("Zn", "Cinc", "seccion3", 12, 4));
+
+contenedor.appendChild(crearElemento("Ga", "Galio", "seccion4", 13, 4));
+contenedor.appendChild(crearElemento("Ge", "Germanio", "seccion5", 14, 4));
+contenedor.appendChild(crearElemento("As", "Arsénico", "seccion5", 15, 4));
+contenedor.appendChild(crearElemento("Se", "Selenio", "seccion6", 16, 4));
+contenedor.appendChild(crearElemento("Br", "Bromo", "seccion7", 17, 4));
+contenedor.appendChild(crearElemento("Kr", "Kriptón", "seccion8", 18, 4));
+
+// Fila 5
+contenedor.appendChild(crearElemento("Rb", "Rubidio", "seccion1", 1, 5));
+contenedor.appendChild(crearElemento("Sr", "Estroncio", "seccion2", 2, 5));
+contenedor.appendChild(crearElemento("Y", "Ytrio", "seccion3", 3, 5));
+
+contenedor.appendChild(crearElemento("Zr", "Circonio", "seccion3", 4, 5));
+contenedor.appendChild(crearElemento("Nb", "Niobio", "seccion3", 5, 5));
+contenedor.appendChild(crearElemento("Mo", "Molibdeno", "seccion3", 6, 5));
+contenedor.appendChild(crearElemento("Tc", "Tecnecio", "seccion3", 7, 5));
+contenedor.appendChild(crearElemento("Ru", "Rutenio", "seccion3", 8, 5));
+contenedor.appendChild(crearElemento("Rh", "Rodio", "seccion3", 9, 5));
+contenedor.appendChild(crearElemento("Pd", "Paladio", "seccion3", 10, 5));
+contenedor.appendChild(crearElemento("Ag", "Plata", "seccion3", 11, 5));
+contenedor.appendChild(crearElemento("Cd", "Cadmio", "seccion3", 12, 5));
+
+contenedor.appendChild(crearElemento("In", "Indio", "seccion4", 13, 5));
+contenedor.appendChild(crearElemento("Sn", "Estaño", "seccion4", 14, 5));
+contenedor.appendChild(crearElemento("Sb", "Antimonio", "seccion5", 15, 5));
+contenedor.appendChild(crearElemento("Te", "Telurio", "seccion5", 16, 5));
+contenedor.appendChild(crearElemento("I", "Yodo", "seccion7", 17, 5));
+contenedor.appendChild(crearElemento("Xe", "Xenón", "seccion8", 18, 5));
+
+// Fila 6
+contenedor.appendChild(crearElemento("Cs", "Cesio", "seccion1", 1, 6));
+contenedor.appendChild(crearElemento("Ba", "Bario", "seccion2", 2, 6));
+contenedor.appendChild(crearElemento("La", "Lantano", "seccion3", 3, 6));
+
+contenedor.appendChild(crearElemento("Hf", "Hafnio", "seccion3", 4, 6));
+contenedor.appendChild(crearElemento("Ta", "Tantalio", "seccion3", 5, 6));
+contenedor.appendChild(crearElemento("W", "Wolframio", "seccion3", 6, 6));
+contenedor.appendChild(crearElemento("Re", "Renio", "seccion3", 7, 6));
+contenedor.appendChild(crearElemento("Os", "Osmio", "seccion3", 8, 6));
+contenedor.appendChild(crearElemento("Ir", "Iridio", "seccion3", 9, 6));
+contenedor.appendChild(crearElemento("Pt", "Platino", "seccion3", 10, 6));
+contenedor.appendChild(crearElemento("Au", "Oro", "seccion3", 11, 6));
+contenedor.appendChild(crearElemento("Hg", "Mercurio", "seccion3", 12, 6));
+
+contenedor.appendChild(crearElemento("Tl", "Talio", "seccion4", 13, 6));
+contenedor.appendChild(crearElemento("Pb", "Plomo", "seccion4", 14, 6));
+contenedor.appendChild(crearElemento("Bi", "Bismuto", "seccion4", 15, 6));
+contenedor.appendChild(crearElemento("Po", "Polonio", "seccion5", 16, 6));
+contenedor.appendChild(crearElemento("At", "Astato", "seccion7", 17, 6));
+contenedor.appendChild(crearElemento("Rn", "Radón", "seccion8", 18, 6));
+
+// Fila 7
+contenedor.appendChild(crearElemento("Fr", "Francio", "seccion1", 1, 7));
+contenedor.appendChild(crearElemento("Ra", "Radio", "seccion2", 2, 7));
+contenedor.appendChild(crearElemento("Ac", "Actinio", "seccion3", 3, 7));
+
+contenedor.appendChild(crearElemento("Rf", "Rutherfordio", "seccion3", 4, 7));
+contenedor.appendChild(crearElemento("Db", "Dubnio", "seccion3", 5, 7));
+contenedor.appendChild(crearElemento("Sg", "Seaborgio", "seccion3", 6, 7));
+contenedor.appendChild(crearElemento("Bh", "Bohrio", "seccion3", 7, 7));
+contenedor.appendChild(crearElemento("Hs", "Hassio", "seccion3", 8, 7));
+contenedor.appendChild(crearElemento("Mt", "Meitnerio", "seccion3", 9, 7));
+contenedor.appendChild(crearElemento("Ds", "Darmstadtio", "seccion3", 10, 7));
+contenedor.appendChild(crearElemento("Rg", "Roentgenio", "seccion3", 11, 7));
+contenedor.appendChild(crearElemento("Cn", "Copernicio", "seccion3", 12, 7));
+
+contenedor.appendChild(crearElemento("Nh", "Nihonio", "seccion4", 13, 7));
+contenedor.appendChild(crearElemento("Fl", "Flerovio", "seccion4", 14, 7));
+contenedor.appendChild(crearElemento("Mc", "Moscovio", "seccion4", 15, 7));
+contenedor.appendChild(crearElemento("Lv", "Livermorio", "seccion4", 16, 7));
+contenedor.appendChild(crearElemento("Ts", "Tenesino", "seccion7", 17, 7));
+contenedor.appendChild(crearElemento("Og", "Oganesón", "seccion8", 18, 7));
